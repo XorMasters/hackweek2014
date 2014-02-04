5be3d934527cf71beef36e38719a9c7ea2a18e6a
Allan Knight <Allan.Knight@citrix.com>
2014-02-04 10:40:40 -0800

    Committing working code
    This code works and allows connection of just a data channel. I’m
    checking it in now as a synch point in case development does not go
    well.

--- DataChannel/step7/js/main.js
+++ DataChannel/step7/js/main.js
@@ -1,5 +1,35 @@
 'use strict';
 
+var citrix = {
+  transport: {
+    DataChannel: function(name) {
+      this.name = name;
+    },
+    
+    Transport: function(name) {
+      this.dataChannel = new citrix.transport.DataChannel(name);
+    }
+  }
+};
+
+citrix.transport.Transport.prototype = {
+  constructor: citrix.transport.Transport,
+  start: function() {
+    startDataChannel();
+  }
+}
+
+citrix.transport.DataChannel.prototype = {
+  constructor: citrix.transport.DataChannel,
+  foo: function() {
+    console.log('foo');
+  },
+        
+  bar: function() {
+    console.log('bar');
+  }
+}
+
 var sendChannel;
 var sendButton = document.getElementById("sendButton");
 var sendTextarea = document.getElementById("dataChannelSend");
@@ -10,9 +40,7 @@ sendButton.onclick = sendData;
 var isChannelReady;
 var isInitiator;
 var isStarted;
-var localStream;
 var pc;
-var remoteStream;
 var turnReady;
 
 var pc_config = webrtcDetectedBrowser === 'firefox' ?
@@ -34,7 +62,6 @@ var sdpConstraints = {'mandatory': {
 
 var room = location.pathname.substring(1);
 if (room === '') {
-//  room = prompt('Enter room name:');
   room = 'foo';
 } else {
   //
@@ -101,34 +128,23 @@ socket.on('message', function (message){
 
 ////////////////////////////////////////////////////
 
-var localVideo = document.querySelector('#localVideo');
-var remoteVideo = document.querySelector('#remoteVideo');
-
-function handleUserMedia(stream) {
-  localStream = stream;
-  attachMediaStream(localVideo, stream);
-  console.log('Adding local stream.');
+function startDataChannel() {
+  console.log('Starting Data Channel.');
   sendMessage('got user media');
   if (isInitiator) {
     maybeStart();
   }
 }
 
-function handleUserMediaError(error){
-  console.log('getUserMedia error: ', error);
-}
+console.log('Boot strapping system');
+var transport = new citrix.transport.Transport("hello");
+transport.start();
 
-var constraints = {video: true};
-
-getUserMedia(constraints, handleUserMedia, handleUserMediaError);
-console.log('Getting user media with constraints', constraints);
-
-requestTurn('https://computeengineondemand.appspot.com/turn?username=41784574&key=4080218913');
+console.log( "Channel name: " + transport.dataChannel.name );
 
 function maybeStart() {
-  if (!isStarted && localStream && isChannelReady) {
+  if (!isStarted && isChannelReady) {
     createPeerConnection();
-    pc.addStream(localStream);
     isStarted = true;
     if (isInitiator) {
       doCall();
@@ -154,14 +170,12 @@ function createPeerConnection() {
     alert('Cannot create RTCPeerConnection object.');
       return;
   }
-  pc.onaddstream = handleRemoteStreamAdded;
-  pc.onremovestream = handleRemoteStreamRemoved;
 
   if (isInitiator) {
     try {
       // Reliable Data Channels not yet supported in Chrome
       sendChannel = pc.createDataChannel("sendDataChannel",
-        {reliable: false});
+        {reliable: true});
       sendChannel.onmessage = handleMessage;
       trace('Created send data channel');
     } catch (e) {
@@ -182,26 +196,6 @@ function sendData() {
   trace('Sent data: ' + data);
 }
 
-// function closeDataChannels() {
-//   trace('Closing data channels');
-//   sendChannel.close();
-//   trace('Closed data channel with label: ' + sendChannel.label);
-//   receiveChannel.close();
-//   trace('Closed data channel with label: ' + receiveChannel.label);
-//   localPeerConnection.close();
-//   remotePeerConnection.close();
-//   localPeerConnection = null;
-//   remotePeerConnection = null;
-//   trace('Closed peer connections');
-//   startButton.disabled = false;
-//   sendButton.disabled = true;
-//   closeButton.disabled = true;
-//   dataChannelSend.value = "";
-//   dataChannelReceive.value = "";
-//   dataChannelSend.disabled = true;
-//   dataChannelSend.placeholder = "Press Start, enter some text, then press Send.";
-// }
-
 function gotReceiveChannel(event) {
   trace('Receive Channel Callback');
   sendChannel = event.channel;
@@ -211,7 +205,7 @@ function gotReceiveChannel(event) {
 }
 
 function handleMessage(event) {
-  trace('Received message: ' + event.data);
+  trace('Received data channel message: ' + event.data);
   receiveTextarea.value = event.data;
 }
 
@@ -252,14 +246,6 @@ function handleIceCandidate(event) {
   }
 }
 
-function handleRemoteStreamAdded(event) {
-  console.log('Remote stream added.');
-//  reattachMediaStream(miniVideo, localVideo);
-  attachMediaStream(remoteVideo, event.stream);
-  remoteStream = event.stream;
-//  waitForRemoteVideo();
-}
-
 function doCall() {
   var constraints = {'optional': [], 'mandatory': {'MozDontOfferDataChannel': true}};
   // temporary measure to remove Moz* constraints in Chrome
@@ -291,50 +277,8 @@ function mergeConstraints(cons1, cons2) {
 }
 
 function setLocalAndSendMessage(sessionDescription) {
-  // Set Opus as the preferred codec in SDP if Opus is present.
-  sessionDescription.sdp = preferOpus(sessionDescription.sdp);
   pc.setLocalDescription(sessionDescription);
   sendMessage(sessionDescription);
-}
-
-function requestTurn(turn_url) {
-  var turnExists = false;
-  for (var i in pc_config.iceServers) {
-    if (pc_config.iceServers[i].url.substr(0, 5) === 'turn:') {
-      turnExists = true;
-      turnReady = true;
-      break;
-    }
-  }
-  if (!turnExists) {
-    console.log('Getting TURN server from ', turn_url);
-    // No TURN server. Get one from computeengineondemand.appspot.com:
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function(){
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        var turnServer = JSON.parse(xhr.responseText);
-      	console.log('Got TURN server: ', turnServer);
-        pc_config.iceServers.push({
-          'url': 'turn:' + turnServer.username + '@' + turnServer.turn,
-          'credential': turnServer.password
-        });
-        turnReady = true;
-      }
-    };
-    xhr.open('GET', turn_url, true);
-    xhr.send();
-  }
-}
-
-function handleRemoteStreamAdded(event) {
-  console.log('Remote stream added.');
- // reattachMediaStream(miniVideo, localVideo);
-  attachMediaStream(remoteVideo, event.stream);
-  remoteStream = event.stream;
-//  waitForRemoteVideo();
-}
-function handleRemoteStreamRemoved(event) {
-  console.log('Remote stream removed. Event: ', event);
 }
 
 function hangup() {
@@ -351,86 +295,7 @@ function handleRemoteHangup() {
 
 function stop() {
   isStarted = false;
-  // isAudioMuted = false;
-  // isVideoMuted = false;
   pc.close();
   pc = null;
-}
-
-///////////////////////////////////////////
-
-// Set Opus as the default audio codec if it's present.
-function preferOpus(sdp) {
-  var sdpLines = sdp.split('\r\n');
-  var mLineIndex;
-  // Search for m line.
-  for (var i = 0; i < sdpLines.length; i++) {
-      if (sdpLines[i].search('m=audio') !== -1) {
-        mLineIndex = i;
-        break;
-      }
-  }
-  if (mLineIndex === null) {
-    return sdp;
-  }
-
-  // If Opus is available, set it as the default in m line.
-  for (i = 0; i < sdpLines.length; i++) {
-    if (sdpLines[i].search('opus/48000') !== -1) {
-      var opusPayload = extractSdp(sdpLines[i], /:(\d+) opus\/48000/i);
-      if (opusPayload) {
-        sdpLines[mLineIndex] = setDefaultCodec(sdpLines[mLineIndex], opusPayload);
-      }
-      break;
-    }
-  }
-
-  // Remove CN in m line and sdp.
-  sdpLines = removeCN(sdpLines, mLineIndex);
-
-  sdp = sdpLines.join('\r\n');
-  return sdp;
-}
-
-function extractSdp(sdpLine, pattern) {
-  var result = sdpLine.match(pattern);
-  return result && result.length === 2 ? result[1] : null;
-}
-
-// Set the selected codec to the first in m line.
-function setDefaultCodec(mLine, payload) {
-  var elements = mLine.split(' ');
-  var newLine = [];
-  var index = 0;
-  for (var i = 0; i < elements.length; i++) {
-    if (index === 3) { // Format of media starts from the fourth.
-      newLine[index++] = payload; // Put target payload to the first.
-    }
-    if (elements[i] !== payload) {
-      newLine[index++] = elements[i];
-    }
-  }
-  return newLine.join(' ');
-}
-
-// Strip CN from sdp before CN constraints is ready.
-function removeCN(sdpLines, mLineIndex) {
-  var mLineElements = sdpLines[mLineIndex].split(' ');
-  // Scan from end for the convenience of removing an item.
-  for (var i = sdpLines.length-1; i >= 0; i--) {
-    var payload = extractSdp(sdpLines[i], /a=rtpmap:(\d+) CN\/\d+/i);
-    if (payload) {
-      var cnPos = mLineElements.indexOf(payload);
-      if (cnPos !== -1) {
-        // Remove CN payload from m line.
-        mLineElements.splice(cnPos, 1);
-      }
-      // Remove CN line in sdp
-      sdpLines.splice(i, 1);
-    }
-  }
-
-  sdpLines[mLineIndex] = mLineElements.join(' ');
-  return sdpLines;
 }
 
