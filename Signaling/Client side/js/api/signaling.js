b17bcd680cd87761e289792c03f2fc48f657bff5
Sagar Joglekar <Sagar@Sagars-MacBook-Pro.local>
2014-02-04 17:22:22 -0800

    changes to play around api

--- Signaling/Client side/js/api/signaling.js
+++ Signaling/Client side/js/api/signaling.js
@@ -7,19 +7,19 @@ var xormasters.signaling = {
 	 var myAgentDataRef = new Firebase("https://xormastersclient.firebaseio.com");
 	 var myClentDataRef = new Firebase("https://blazing-fire-5145.firebaseio.com");
 
-	postRequest : function( request ) {
+	this.postRequest : function( request ) {
 		myAgentDataRef.push(request);
 	}
 
-	postClientRequest : function(request) {
+	this.postClientRequest : function(request) {
 		myClentDataRef.push(request);
 	}
 
-	acceptRequest : function(request , response) {
+	this.acceptRequest : function(request , response) {
 
 	}
 
-	on : function(event , request) {
+	this.on : function(event , request) {
 
 	}
 
