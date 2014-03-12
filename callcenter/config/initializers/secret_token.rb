4ebba8e1068ccde7900a5af87ecc5614d2355bf2
Allan Knight <AllanKnight@citrix.com>
2014-03-12 16:07:00 +0000

    Rest of Ruby on Rails files

---
+++ callcenter/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Callcenter::Application.config.secret_key_base = '7acda24e5b90149e252eb25ab225cda7bb8d6e652a82c8dfbb45f8eabaa3e898038b671c28c6fdb9dd91be1193deb3aae40d787ee9f3c7e91c791a44b9b2886c'
---
+++ callcenter/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Callcenter::Application.config.session_store :cookie_store, key: '_callcenter_session'
